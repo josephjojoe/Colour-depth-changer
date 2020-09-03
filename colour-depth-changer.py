ab5b633d6dd54bfb5089b86851acb42974be961d
Joseph Jojoe <joseph.jojoe101@gmail.com>
2020-09-03 15:36:51 +0100

    Add files via upload
    Added my first function, image_depth_gif(), which converts a given image to a GIF showing the progression of reduced colour depth.

--- colour-depth-changer.py
+++ colour-depth-changer.py
@@ -1,10 +1,17 @@
 from PIL import Image
 import imageio
 
+# This function uses an adaptive palette
+def image_depth_gif(pathToImage, frameDuration=500): 
+    originalImage = Image.open(pathToImage)
+    frames = []
+    amountOfColours = 256
+    while amountOfColours >= 2:
+        imageWithColourPalette = originalImage.convert("P", palette=Image.ADAPTIVE, colors=amountOfColours)
+        amountOfColours = int(amountOfColours / 2) # Program reads amountOfColours as float with /= 2 which doesn't work
+        frames.append(imageWithColourPalette)
+    frames[0].save('color-depth-change.gif', format='GIF', append_images=frames[1:], save_all=True, duration=frameDuration, loop=0)
 
-originalImage = Image.open("./painting.jpg")
-originalImage.show()
-
-
-imageWithColorPalette = originalImage.convert("P", palette=Image.ADAPTIVE, colors=128)
-imageWithColorPalette.show()
+def gif_depth_change(pathToGIF):
+    originalGIF = Image.open(pathToGIF)
+    ### to do
